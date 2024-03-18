b2402950e5a9c93416da1f22a2ce06f292cfa87b
fadhlaouir <raed.fadhlaoui@hotmail.com>
2024-03-18 23:21:29 +0100

    chore: Fixes

--- boilerplate/cli/index.js
+++ boilerplate/cli/index.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
+// Import required modules
 const { exec } = require("child_process");
 const path = require("path");
 const inquirer = require("inquirer");
 const fse = require("fs-extra");
 const ora = require("ora");
-const chalk = require("chalk"); // Import chalk for colored output
+const chalk = require("chalk");
 
-// Define the directory paths
-const boilerplateDir = path.join(__dirname, "..");
-const templatesDir = path.join(boilerplateDir, "templates");
-
-// Main function to create the project
+/**
+ * Main function to create a new Express.js project.
+ */
 async function main() {
   try {
     // Prompt user for project name and directory
@@ -20,26 +19,36 @@ async function main() {
     // Display loading message
     const loading = createLoading("Creating project...");
 
-    // Copy template to new project directory
-    copyTemplate(projectDirectory);
+    // Define directory paths
+    const boilerplateDir = path.join(__dirname, "..");
+    const templatesDir = path.join(boilerplateDir, "template");
 
-    // Install dependencies
+    // Copy template to new project directory
+    copyTemplate(templatesDir, projectDirectory);
+
+    // Install project dependencies
     await installDependencies(projectDirectory);
 
+    // Display success message
     loading.succeed(chalk.green("Project created successfully"));
     console.log(
       `Project '${projectName}' created successfully in ${projectDirectory}`
     );
   } catch (error) {
+    // Handle errors
     console.error(chalk.red("An error occurred:"));
     console.error(chalk.red(error.message || error));
   }
 }
 
-// Function to prompt user for project details
+/**
+ * Function to prompt user for project details.
+ * @returns {Object} An object containing project name and directory.
+ */
 async function promptProjectDetails() {
   console.log("\n🚀 Let's create a new Express.js project!");
 
+  // Define questions for user input
   const questions = [
     {
       type: "input",
@@ -55,30 +64,49 @@ async function promptProjectDetails() {
     },
   ];
 
+  // Prompt user for input and return the responses
   return inquirer.prompt(questions);
 }
 
-// Function to copy template to new project directory
-function copyTemplate(projectDirectory) {
+/**
+ * Function to copy template to new project directory.
+ * @param {string} templatesDir - Path to the template directory.
+ * @param {string} projectDirectory - Path to the project directory.
+ */
+function copyTemplate(templatesDir, projectDirectory) {
+  // Calculate destination path
   const destinationPath = path.join(process.cwd(), projectDirectory);
+
+  // Copy template files to destination directory
   fse.copySync(templatesDir, destinationPath);
 }
 
-// Function to install project dependencies
+/**
+ * Function to install project dependencies.
+ * @param {string} projectDirectory - Path to the project directory.
+ * @returns {Promise<void>} A promise representing the installation process.
+ */
 async function installDependencies(projectDirectory) {
   return new Promise((resolve, reject) => {
     console.log("\nInstalling dependencies...");
+
+    // Execute npm install command in project directory
     const child = exec(`cd ${projectDirectory} && npm install`);
+
+    // Capture stdout and stderr
     child.stdout.on("data", (data) => {
       console.log(data.toString());
     });
     child.stderr.on("data", (data) => {
-      console.error(chalk.yellow(data.toString())); // Display warnings in yellow
+      console.error(chalk.yellow(data.toString()));
     });
+
+    // Handle process exit
     child.on("exit", (code) => {
       if (code === 0) {
-        resolve();
+        resolve(); // Resolve promise on successful installation
       } else {
+        // Reject promise with error message
         reject(
           new Error(
             chalk.red(`Failed to install dependencies with exit code ${code}`)
@@ -89,9 +117,15 @@ async function installDependencies(projectDirectory) {
   });
 }
 
-// Function to create a loading indicator
+/**
+ * Function to create a loading indicator.
+ * @param {string} message - Loading message.
+ * @returns {Object} Loading spinner instance.
+ */
 function createLoading(message) {
+  // Create and start loading spinner
   return ora({ text: message, spinner: "dots" }).start();
 }
 
+// Entry point: Call the main function
 main();
